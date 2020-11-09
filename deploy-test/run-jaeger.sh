f1fd6ebadac56f01bcacab504aa81d5dc6b89e0d
‘boylei’ <1119016521@qq.com>
2020-11-09 23:04:28 +0800

    make a python image #MCS-95 smell实验

--- deploy-test/run-jaeger.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose -f user-compose-withjager.yml kill
-docker ps -a | grep jaegertest | awk '{print $1}' | xargs docker rm -f
-docker-compose -f user-compose-withjager.yml -p jaegertest build
-docker-compose -f user-compose-withjager.yml -p jaegertest up
