77ef1a51266ba905420c754db3e0b3cf34446d0b
‘boylei’ <1119016521@qq.com>
2020-11-11 13:52:34 +0800

    change the run.sh #MCS-95 smell实验

--- deploy-k8s-test/run.sh
+++ deploy-k8s-test/run.sh
@@ -6,4 +6,9 @@ kubectl delete pods --all
 
 kubectl apply -f zipkin.yaml
 kubectl apply -f ts-deploy-before.yaml
+
+echo 'sleep 1 minute'
+sleep 1m
+
+echo 'deploy the service'
 kubectl apply -f ts-deploy-after.yaml
