ea6e8c4d3aca691c7ba52c1714545bb45402457e
‘boylei’ <1119016521@qq.com>
2020-11-30 19:43:22 +0800

    debug #MCS-95 smell实验

--- run_test.sh
+++ run_test.sh
@@ -7,15 +7,15 @@ docker images | grep alltest | awk '{print $3}' | xargs docker rmi -f
 docker-compose -f docker-compose.yml -p alltest build
 docker-compose -f docker-compose.yml -p alltest up
 
-#sleep 1m
-#
-#echo 'initDataPrice'
-#curl 'http://localhost:18000/initDataPrice'
-#
-#sleep 5s
-#echo 'initDataTravel'
-#curl 'http://localhost:18000/initDataTravel'
-#
-#sleep 5s
-#echo 'initDataUser'
-#curl 'http://localhost:18000/initDataUser'
+sleep 1m
+
+echo 'initDataPrice'
+curl 'http://localhost:18000/initDataPrice'
+
+sleep 5s
+echo 'initDataTravel'
+curl 'http://localhost:18000/initDataTravel'
+
+sleep 5s
+echo 'initDataUser'
+curl 'http://localhost:18000/initDataUser'
