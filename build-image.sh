b6e4664a842ce034c04b9d7b610ad49c5b44c05b
‘boylei’ <1119016521@qq.com>
2020-12-09 10:03:42 +0800

    移除zipkin，未来整合framework #MCS-95 smell实验

--- build-image.sh
+++ build-image.sh
@@ -5,16 +5,16 @@ mvn clean package -Dmaven.test.skip=true
 
 # delete docker image
 echo 'delete docker image'
-docker images | grep leizipkin | awk '{print $1}' | xargs docker rmi
-docker images | grep leizipkin | awk '{print $3}' | xargs docker rmi
+docker images | grep framework | awk '{print $1}' | xargs docker rmi
+docker images | grep framework | awk '{print $3}' | xargs docker rmi
 # build build image
 echo ' build the image'
-docker-compose -f docker-compose.yml -p leizipkin build
+docker-compose -f docker-compose.yml -p framework build
 
 # tag image
 echo 'tag the image'
-docker images | grep leizipkin | awk '{print "docker tag"" "$1":"$2 " ""192.168.1.104:5000/"$1":"$2}'| sh
+docker images | grep framework | awk '{print "docker tag"" "$1":"$2 " ""192.168.1.104:5000/"$1":"$2}'| sh
 
 # docker push
 echo 'docker push to the registry'
-docker images | grep 192.168.1.104:5000/leizipkin | awk '{print "docker push"" "$1":"$2}' | sh
+docker images | grep 192.168.1.104:5000/framework | awk '{print "docker push"" "$1":"$2}' | sh
