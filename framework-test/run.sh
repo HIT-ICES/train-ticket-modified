ad96143add4b0d5ce7d010eb313335d1bbc6af2c
‘boylei’ <1119016521@qq.com>
2020-12-11 15:50:19 +0800

    test framework #MCS-95 smell实验

--- framework-test/run.sh
+++ framework-test/run.sh
@@ -1,3 +1,12 @@
 #!/bin/zsh
 
 echo 'framework test'
+
+kubectl delete service --all -n default
+kubectl delete deployment --all -n default
+kubectl delete pods --all -n default
+
+
+kubectl apply -f before.yaml
+
+kubectl apply -f after.yaml
