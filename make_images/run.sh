108823ddc9ee5e5f018bbf6553b7b786259bc3a0
liulei <1119016521@qq.com>
2021-01-30 15:16:00 +0800

    更改 #MCS-95 smell实验

--- make_images/run.sh
+++ make_images/run.sh
@@ -1,6 +1,6 @@
-#!/bin/zsh
+#!/bin/bash
 
 docker rmi lei_python:3
 docker build -t lei_python:3 .
-docker tag lei_python:3 192.168.1.104:5000/lei_python:3
-docker push 192.168.1.104:5000/lei_python:3
+docker tag lei_python:3 172.31.15.247:5000/lei_python:3
+docker push 172.31.15.247:5000/lei_python:3
