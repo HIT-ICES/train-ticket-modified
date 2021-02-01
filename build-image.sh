08d7a8043e7ca56f9a2c3adac515a6cc5ce03a78
liulei <1119016521@qq.com>
2021-02-01 17:38:22 +0800

    更改 #MCS-95 smell实验

--- build-image.sh
+++ build-image.sh
@@ -12,7 +12,7 @@ echo ' build the image'
 docker-compose -f docker-compose.yml -p framework build
 
 echo 'tag the image'
-docker images | grep framework | awk '{print "docker tag"" "$1":"$2 " ""172.31.15.247:5000/"$1":"$2}'| sh
+docker images | grep framework | awk '{print "docker tag"" "$1":"$2 " ""172.31.22.168:5000/"$1":"$2}'| sh
 
 echo 'docker push to the registry'
-docker images | grep 172.31.15.247:5000/framework | awk '{print "docker push"" "$1":"$2}' | sh
+docker images | grep 172.31.22.168:5000/framework | awk '{print "docker push"" "$1":"$2}' | sh
