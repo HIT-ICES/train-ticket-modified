fe2835e155d5be5c7573a84a19022341ad7df698
‘boylei’ <1119016521@qq.com>
2021-12-06 16:30:19 +0800

    更改 #MCS-95 smell实验

--- ts-consign-price-service/src/main/java/consignprice/ConsignPriceApplication.java
+++ ts-consign-price-service/src/main/java/consignprice/ConsignPriceApplication.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.wangteng.mclient.annotation.MClient;
+import com.ll.framework.ano.MClient;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
